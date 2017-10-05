eceb369dd7b854cb180efb60270330020c4f1d87
Lilian Sheu <lilians@umich.edu>
2017-10-05 16:50:48 -0400

    second commit

---
+++ hw5.py
@@ -0,0 +1,17 @@
+# Name: Lilian Sheu
+# Discussion: Thursdays 3-4 pm
+
+import re
+
+f = open('regex_sum_31740.txt')
+list1 = []
+sum = 0
+for line in f:
+	line = line.rstrip()
+	y = re.findall('[0-9]+', line)
+	if len(y) > 0:	
+		for i in y:
+			sum += int(i)
+print (sum)
+
+#print (sum([y in re.findall('[0-9]+', 'regex_sum_31740.txt'.read())]))
